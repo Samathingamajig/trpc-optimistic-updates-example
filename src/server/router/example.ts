14d331383e0b186324bf97668810c2826a1410a9
Samuel Gunter <samathingamajig@gmail.com>
2022-10-02 00:20:14 -0500

    Simple example

--- src/server/router/example.ts
+++ src/server/router/example.ts
@@ -1,15 +1,56 @@
 import { createRouter } from "./context";
 import { z } from "zod";
+import { TRPCClientError } from "@trpc/client";
 
-export const exampleRouter = createRouter().query("hello", {
-  input: z
-    .object({
-      text: z.string().nullish(),
-    })
-    .nullish(),
-  resolve({ input }) {
-    return {
-      greeting: `Hello ${input?.text ?? "world"}`,
-    };
-  },
-});
+export interface Todo {
+  id: number;
+  text: string;
+  done: boolean;
+}
+
+const todos = [
+  { id: 1, text: "First", done: true },
+  { id: 2, text: "Second", done: false },
+  { id: 3, text: "Third", done: false },
+];
+
+export const exampleRouter = createRouter()
+  .query("hello", {
+    input: z
+      .object({
+        text: z.string().nullish(),
+      })
+      .nullish(),
+    resolve({ input }) {
+      return {
+        greeting: `Hello ${input?.text ?? "world"}`,
+      };
+    },
+  })
+  .query("todos", {
+    resolve() {
+      return todos;
+    },
+  })
+  .mutation("toggleTodo", {
+    input: z.object({
+      id: z.number(),
+    }),
+    async resolve({ input }) {
+      const todo = todos.find((todo) => todo.id === input.id);
+      if (!todo) {
+        // throw new Error("Todo not found");
+        throw new TRPCClientError("Todo not found", {
+          result: todo,
+        });
+      }
+
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      if (Math.random() > 0.5 || input.id === 3) {
+        throw new Error("Random error");
+      }
+
+      todo.done = !todo.done;
+      return todo;
+    },
+  });
